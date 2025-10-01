20de353acb87f72973707e52658f01216d32530d
vaskomet <metvassilis@gmail.com>
2025-10-01 16:37:23 +0300

    Fix: Resolve server run errors and apply initial migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -67,6 +67,23 @@ class User(AbstractBaseUser, PermissionsMixin):
     # Future field placeholder
     # subscription_tier = models.CharField(max_length=50, blank=True, null=True)
 
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name='groups',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name='user permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_name="custom_user_set",
+        related_query_name="user",
+    )
+
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
